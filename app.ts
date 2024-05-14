8a70af1c162ec783ec91aff4d2399527b919922e
Erez Porat <erezporatwork@gmail.com>
2024-05-14 20:53:14 +0300

    added input function

---
+++ app.ts
@@ -0,0 +1,14 @@
+console.log(`
+_____ ___    ____   ___  
+|_   _/ _ \\  |  _ \\ / _ \\ 
+  | || | | | | | | | | | |
+  | || |_| | | |_| | |_| |
+  |_| \\___/  |____/ \\___/ 
+`);
+
+let flag:boolean = false;
+
+//while(!flag){
+  //  console.log('[S]how [A]dd [C]omplete [Q]uit ');
+
+//}
